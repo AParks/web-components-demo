deaf0776c54258a8959cfe4cb1664d56209c5502
A Parks <parksa@seas.upenn.edu>
2016-07-20 15:07:00 -0500

    conditionally load polyfills

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
-console.log('This would be the main JS file.');
+(function() {
+    if ('registerElement' in document
+        && 'import' in document.createElement('link')
+        && 'content' in document.createElement('template')) {
+      // platform is good!
+    } else {
+      // polyfill the platform!
+      var e = document.createElement('script');
+      e.type = 'text/javascript';
+      e.src = 'https://rawgit.com/webcomponents/webcomponentsjs/master/webcomponents.js';
+      document.body.appendChild(e);
+    }
+})();
