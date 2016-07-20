d5e59753ee8ce2da07302f7d4412304daeb8150c
A Parks <parksa@seas.upenn.edu>
2016-07-20 15:19:10 -0500

    conditionally load polyfill

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,11 +3,19 @@
         && 'import' in document.createElement('link')
         && 'content' in document.createElement('template')) {
       // platform is good!
+      lazyLoadShadowDOM();
     } else {
       // polyfill the platform!
       var e = document.createElement('script');
       e.type = 'text/javascript';
       e.src = 'https://rawgit.com/webcomponents/webcomponentsjs/master/webcomponents.js';
-      document.head.appendChild(e);
+      e.onload = 'lazyLoadShadowDOM';
+      document.body.appendChild(e);
+    }
+    
+    function lazyLoadShadowDOM() {
+      // create shadow DOM on the <p> element above
+      var shadow = document.querySelector('#namespacedPlugin').createShadowRoot();
+      shadow.innerHTML = '<p class="red">Here is some new text with the \'red\' class in the shadow DOM.</p>';
     }
 })();
