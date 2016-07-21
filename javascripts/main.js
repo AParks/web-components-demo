e64f69cad967958012222337915462da56683d1b
A Parks <parksa@seas.upenn.edu>
2016-07-21 11:15:28 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,13 +3,20 @@
         && 'import' in document.createElement('link')
         && 'content' in document.createElement('template')) {
       // platform is good!
+      window.onload = createShadowRoot;
     } else {
       // polyfill the platform!
       var script = "<script type='text/javascript' src='bower_components/webcomponentsjs/webcomponents.js'></script>";
       document.write(script);
+      window.addEventListener('WebComponentsReady', function(e) {
+          // imports are loaded and elements have been registered
+          console.log('Components are ready');
+          var shadowRoot = createShadowRoot();
+          WebComponents.ShadowCSS.shimStyling( shadowRoot, '#namespacedPlugin' );
+      });
     }
     
-    window.onload = function() {
+    function createShadowRoot() {
       // create shadow DOM on the <p> element above
       var shadow = document.querySelector('#namespacedPlugin').createShadowRoot();
       
@@ -21,5 +28,6 @@
       //var style = document.createElement('style');
         //style.textContent = "@import 'stylesheets/stylesheet.css'";
         //shadow.insertBefore(style, shadow.childNodes[0]);
+        return shadow;
     };
 })();
