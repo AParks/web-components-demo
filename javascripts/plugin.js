100eb02cd3745f03f23e33f8c22815d62ac21f47
A Parks <parksa@seas.upenn.edu>
2016-08-09 15:20:44 -0500

    Update plugin.js

--- javascripts/plugin.js
+++ javascripts/plugin.js
@@ -1,7 +1,7 @@
-(function () {
+// (function () {
   console.log(document.querySelector("button.pill"));
 
   document.querySelector("button.pill").addEventListener("click", function() {
     console.log( 'hello');
   });
-});
+// });
